167a04ddb095b3b7953aaa355f6518c2e45d21bd
jbalaraman <james.balaraman@gmail.com>
2023-11-25 18:26:40 -0700

    Explored .join operator

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,7 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 2:
+        return ' and '.join(items)
+    if len(items) >= 3:
+        first_strings = ', '.join(items[:-1])
+        return ', and '.join([first_strings, items[-1]])
+    return ''.join(items)
